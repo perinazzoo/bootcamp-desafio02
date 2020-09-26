5d84364853b0c62cbcbb1b49f22499b2d99dbed1
Gabriel Perinazzo <perinazzo.gbrl@gmail.com>
2020-09-26 17:42:07 -0300

    :construction: add create repository route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,23 +9,35 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get("/repositories", (req, res) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    title,
+    url,
+    techs,
+    id: uuid(),
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  res.json(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
