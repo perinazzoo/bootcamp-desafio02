1cb1be174c253b49f8e89ac3cc4bd5227fa8cb7a
Gabriel Perinazzo <perinazzo.gbrl@gmail.com>
2020-09-26 18:35:23 -0300

    :construction: add edit and delete repository routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuid } = require('uuid');
+const { isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function isValidUuid(req, res, next) {
+  const { id } = req.params;
+
+  const isValid = isUuid(id);
+
+  if (!isValid) {
+    return res.status(400).json({ error: 'O ID não é válido' });
+  }
+
+  next();
+}
+
 const repositories = [];
+
+app.use('/repositories/:id', isValidUuid);
 
 app.get("/repositories", (req, res) => {
   res.json(repositories);
@@ -30,11 +45,40 @@ app.post("/repositories", (req, res) => {
 });
 
 app.put("/repositories/:id", (req, res) => {
-  // TODO
+  const { title, url, techs } = req.body;
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+  const repository = repositories.find(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repositório não existe' });
+  }
+
+  const newRepositoryData = {
+    ...repository,
+    title: title ? title : repository.title,
+    url: url ? url : repository.url,
+    techs: techs ? techs : repository.techs,
+  }
+
+  repositories[repositoryIndex] = newRepositoryData;
+
+  res.json(newRepositoryData);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repositório não existe' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (req, res) => {
